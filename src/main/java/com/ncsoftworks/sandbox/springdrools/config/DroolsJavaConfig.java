531f8b73db2237ebe06c107016263716470ae19d
Nick Cavallo <ncavallo33@gmail.com>
2015-04-26 13:04:40 -0400

    Squashed commit of the following:
    commit eccaec174f5c8d422313ee70f6493251c9a78302
    Author: Nick Cavallo <ncavallo33@gmail.com>
    Date:   Sun Apr 26 12:56:01 2015 -0400
    
        Adding Drools6 support

--- src/main/java/com/ncsoftworks/sandbox/springdrools/config/DroolsJavaConfig.java
+++ src/main/java/com/ncsoftworks/sandbox/springdrools/config/DroolsJavaConfig.java
@@ -1,13 +1,17 @@
 package com.ncsoftworks.sandbox.springdrools.config;
 
-import org.drools.KnowledgeBase;
-import org.drools.builder.*;
-import org.drools.builder.conf.impl.DecisionTableConfigurationImpl;
-import org.drools.container.spring.beans.StatelessKnowledgeSessionBeanFactory;
-import org.drools.io.ResourceFactory;
-import org.drools.runtime.StatelessKnowledgeSession;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.Message;
+import org.kie.api.builder.Results;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.StatelessKieSession;
+import org.kie.spring.KModuleBeanFactoryPostProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -16,25 +20,35 @@ import org.springframework.context.annotation.Profile;
  */
 @Profile("javaconfig")
 @Configuration
+@ComponentScan(basePackages = { "org.kie.spring.annotations" })
 public class DroolsJavaConfig {
-    @Bean(name = "kbase")
-    public KnowledgeBase kbase() {
-        DecisionTableConfigurationImpl decisionTableConfig = new DecisionTableConfigurationImpl();
-        decisionTableConfig.setInputType(DecisionTableInputType.XLS);
-        decisionTableConfig.setWorksheetName("Tables");
-        KnowledgeBuilder builder = KnowledgeBuilderFactory.newKnowledgeBuilder();
-        builder.add(ResourceFactory.newClassPathResource("rules/org/drools/examples/ExamplePolicyPricing.xls"),
-                ResourceType.DTABLE,
-                decisionTableConfig);
 
-        return builder.newKnowledgeBase();
+    @Bean(name = "kbase")
+    public KieBase kbase() {
+        KieServices kieServices = KieServices.Factory.get();
+        KieFileSystem kfs = kieServices.newKieFileSystem();
+        kfs.write(kieServices.getResources().newClassPathResource("rules/org/drools/examples/ExamplePolicyPricing.xls"));
+
+        KieBuilder builder = kieServices.newKieBuilder(kfs);
+        Results results = builder.buildAll().getResults();
+
+        if (results.hasMessages(Message.Level.ERROR)) {
+            throw new IllegalStateException(results.getMessages(Message.Level.ERROR).toString());
+        }
+
+        KieContainer kieContainer = kieServices.newKieContainer(kieServices.getRepository().getDefaultReleaseId());
+
+        return kieContainer.getKieBase();
     }
 
     @Autowired
-    @Bean(name = "statelessSession")
-    public StatelessKnowledgeSessionBeanFactory statelessKnowledgeSession(KnowledgeBase knowledgeBase) throws Exception {
-        StatelessKnowledgeSessionBeanFactory factory = new StatelessKnowledgeSessionBeanFactory();
-        factory.setKbase(knowledgeBase);
-        return factory;
+    @Bean(name = "statelessKieSession")
+    public StatelessKieSession statelessKieSession(KieBase kieBase) throws Exception {
+        return kieBase.newStatelessKieSession();
+    }
+
+    @Bean
+    public KModuleBeanFactoryPostProcessor kModuleBeanFactoryPostProcessor() {
+        return new KModuleBeanFactoryPostProcessor();
     }
 }
